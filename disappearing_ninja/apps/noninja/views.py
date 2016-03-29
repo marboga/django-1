348160f58d663b227abefadd675231b029f4e65a
Michael Arbogast <marboga@gmail.com>
2016-03-28 18:51:33 -0700

    final changes added.

--- disappearing_ninja/apps/noninja/views.py
+++ disappearing_ninja/apps/noninja/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def noninja(request):
+    return HttpResponse("No Ninjas Here. Try looking at /ninja")
--- disappearing_ninja/disappearing_ninja/urls.py
+++ disappearing_ninja/disappearing_ninja/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^ninja/', include('apps.ninja.urls')),
-    # url(r'^', include('apps.ninja.urls')),
+    url(r'^$', include('apps.noninja.urls')),
 ]
